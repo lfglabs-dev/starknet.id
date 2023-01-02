6d76368cd39fb03417ab1a241fe1ebbc524caca8
OxBonaparte <>
2023-01-02 16:00:39 +0100

    ref: add better linter rules

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import Navbar from "../components/navbar";
 import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -8,8 +8,26 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Starknet.id</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#2CAA6E" />
+        <meta
+          property="og:site_name"
+          content="StarkNet identity and naming service"
+        />
+        <meta property="og:title" content="Starknet.ID" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:description"
+          content="Identity and naming service for Starknet. Mint your identity and choose your .stark domain now 🔥"
+        />
+        <meta property="og:url" content="https://starknet.id/" />
+        <meta property="og:image" content="/visuals/StarknetIdLogo.png" />
+        <meta
+          name="description"
+          content="Use your Starknet ID to prove your identity on-chain."
+        />
       </Head>
-      <Navbar />
       <Component {...pageProps} />
     </>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,188 +1,191 @@
-/* eslint-disable @next/next/no-img-element */
+import React from "react";
 import type { NextPage } from "next";
 import Footer from "../components/footer";
 import Roadmap from "../components/roadmap";
 import styles from "../styles/Home.module.css";
 import CarbonableIcon from "../components/icons/carbonable";
 import MintsquareIcon from "../components/icons/mintsquare";
-import FocusTreeIcon from "../components/icons/focustree";
 import CartridgeIcon from "../components/icons/cartridge";
 import RealmsIcon from "../components/icons/realms";
+import Navbar from "../components/navbar";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.section1}>
-        <div className={styles.firstLeaf}>
-          <img width="100%" alt="leaf" src="/leaves/leaf_2.png" />
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <div className={styles.section1}>
+          <div className={styles.firstLeaf}>
+            <img width="100%" alt="leaf" src="/leaves/leaf_2.png" />
+          </div>
+          <div className={styles.secondLeaf}>
+            <img width="100%" alt="leaf" src="/leaves/leaf_1.png" />
+          </div>
+          <div className="flex flex-col items-center justify-center text min-h-screen mx-10">
+            <h1 className={styles.title}>Starknet.id</h1>
+            <h3 className={styles.subtitle}>
+              &rdquo;All in one identity service on starknet&rdquo;
+            </h3>
+          </div>
         </div>
-        <div className={styles.secondLeaf}>
-          <img width="100%" alt="leaf" src="/leaves/leaf_1.png" />
+        <div id="info" className={styles.section2}>
+          <div className={styles.subsection}>
+            <div className={styles.card}>
+              <div className="flex justify-center">
+                <img
+                  alt="identicon"
+                  height={300}
+                  width={300}
+                  src="/visuals/passeport.png"
+                />
+              </div>
+              <h2 className={styles.secondTitle}>
+                #1 Claim your Starknet identity
+              </h2>
+              <div className="text-md max-w-3xl mt-4 sm:mt-10">
+                <p className="text-xl">
+                  You can already mint your starknet identity for free,
+                  it&apos;ll act as your starknet passeport and represent you
+                  during your on-chain interactions.
+                </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className="flex justify-center">
+                <img
+                  alt="identicon"
+                  height={300}
+                  width={300}
+                  src="/visuals/passeportWithUsername.png"
+                />
+              </div>
+              <h2 className={styles.secondTitle}>
+                #2 Register your .stark domain
+              </h2>
+              <div className="text-md max-w-3xl mt-4 sm:mt-10">
+                <p className="text-xl">
+                  You’re not 0x072d4…b2Be7, you can already register your .stark
+                  domain and link it with your starknet identity.
+                </p>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <div className="flex justify-center">
+                <img
+                  alt="identicon"
+                  height={300}
+                  width={300}
+                  src="/visuals/passeportWithEverything.png"
+                />
+              </div>
+              <h2 className={styles.secondTitle}>#3 Build your identity</h2>
+              <div className="text-md max-w-3xl mt-4 sm:mt-10">
+                <p className="text-xl">
+                  You&apos;ll also able to attach any data to your starknet
+                  identity, from web2 with social media accounts to web3 with
+                  ENS domains.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className={styles.subsection}>
+            <div className={styles.card2}>
+              <h2 className={styles.secondTitle}>They are already using us</h2>
+              <div className={styles.partners}>
+                {/* <BriqIcon /> */}
+                {/* <StarkwareIcon /> */}
+                <a href="https://carbonable.io/">
+                  <CarbonableIcon />
+                </a>
+
+                <a href="https://onlydust.xyz/">
+                  <img
+                    height={100}
+                    width={100}
+                    src="/visuals/partners/OnlyDustLogo.png"
+                    alt="only dust logo"
+                    className="m-5"
+                  />
+                </a>
+
+                <a href="https://mintsquare.io/">
+                  <div className="flex items-center justify-center m-5">
+                    <MintsquareIcon />
+                    <h3 className={styles.mintsquareTitle}>Mintsquare</h3>
+                  </div>
+                </a>
+                <a href="https://eykar.org/">
+                  <img
+                    className="m-5"
+                    height={100}
+                    width={100}
+                    src="/visuals/partners/EykarLogo.svg"
+                    alt="Eykar logo"
+                  />
+                </a>
+
+                <a href="https://focustree.app/">
+                  <img
+                    className="m-5 rounded-xl"
+                    height={100}
+                    width={100}
+                    src="/visuals/partners/focustreeLogo.png"
+                    alt="Imperium Wars logo"
+                  />{" "}
+                </a>
+
+                <a href="https://cartridge.gg/">
+                  <CartridgeIcon />
+                </a>
+                <a href="https://imperiumwars.xyz/">
+                  <img
+                    className="m-5 rounded-xl"
+                    height={100}
+                    width={100}
+                    src="/visuals/partners/ImperiumWarsLogo.png"
+                    alt="Imperium Wars logo"
+                  />
+                </a>
+
+                <a href="https://www.frenslands.xyz/">
+                  <img
+                    className="m-5 rounded-xl"
+                    height={120}
+                    width={120}
+                    src="/visuals/partners/FrenslandsLogo.png"
+                    alt="Frenslands logo"
+                  />
+                </a>
+
+                <a href="https://www.earlystarkers.io/">
+                  <img
+                    className="m-5 rounded-xl"
+                    height={180}
+                    width={180}
+                    src="/visuals/partners/EarlyStarkersLogo.svg"
+                    alt="Early Starkers logo"
+                  />
+                </a>
+
+                <a href="https://realmseternum.com/">
+                  <RealmsIcon />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col items-center justify-center text min-h-screen mx-10">
-          <h1 className={styles.title}>Starknet.id</h1>
-          <h3 className={styles.subtitle}>
-            &rdquo;All in one identity service on starknet&rdquo;
-          </h3>
+        <div className={styles.section3}>
+          <div className={styles.thirdLeaf}>
+            <img width={"100%"} alt="leaf" src="/leaves/leaf_2.png" />
+          </div>
+          <div className={styles.fourthLeaf}>
+            <img width={"100%"} alt="leaf" src="/leaves/leaf_1.png" />
+          </div>
+          <Roadmap />
         </div>
+        <Footer />
       </div>
-      <div id="info" className={styles.section2}>
-        <div className={styles.subsection}>
-          <div className={styles.card}>
-            <div className="flex justify-center">
-              <img
-                alt="identicon"
-                height={300}
-                width={300}
-                src="/visuals/passeport.png"
-              />
-            </div>
-            <h2 className={styles.secondTitle}>
-              #1 Claim your Starknet identity
-            </h2>
-            <div className="text-md max-w-3xl mt-4 sm:mt-10">
-              <p className="text-xl">
-                You can already mint your starknet identity for free, it&apos;ll
-                act as your starknet passeport and represent you during your
-                on-chain interactions.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className="flex justify-center">
-              <img
-                alt="identicon"
-                height={300}
-                width={300}
-                src="/visuals/passeportWithUsername.png"
-              />
-            </div>
-            <h2 className={styles.secondTitle}>
-              #2 Register your .stark domain
-            </h2>
-            <div className="text-md max-w-3xl mt-4 sm:mt-10">
-              <p className="text-xl">
-                You’re not 0x072d4…b2Be7, you can already register your .stark
-                domain and link it with your starknet identity.
-              </p>
-            </div>
-          </div>
-          <div className={styles.card}>
-            <div className="flex justify-center">
-              <img
-                alt="identicon"
-                height={300}
-                width={300}
-                src="/visuals/passeportWithEverything.png"
-              />
-            </div>
-            <h2 className={styles.secondTitle}>#3 Build your identity</h2>
-            <div className="text-md max-w-3xl mt-4 sm:mt-10">
-              <p className="text-xl">
-                You&apos;ll also able to attach any data to your starknet
-                identity, from web2 with social media accounts to web3 with ENS
-                domains.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className={styles.subsection}>
-          <div className={styles.card2}>
-            <h2 className={styles.secondTitle}>They are already using us</h2>
-            <div className={styles.partners}>
-              {/* <BriqIcon /> */}
-              {/* <StarkwareIcon /> */}
-              <a href="https://carbonable.io/">
-                <CarbonableIcon />
-              </a>
-
-              <a href="https://onlydust.xyz/">
-                <img
-                  height={100}
-                  width={100}
-                  src="/visuals/partners/OnlyDustLogo.png"
-                  alt="only dust logo"
-                  className="m-5"
-                />
-              </a>
-
-              <a href="https://mintsquare.io/">
-                <div className="flex items-center justify-center m-5">
-                  <MintsquareIcon />
-                  <h3 className={styles.mintsquareTitle}>Mintsquare</h3>
-                </div>
-              </a>
-              <a href="https://eykar.org/">
-                <img
-                  className="m-5"
-                  height={100}
-                  width={100}
-                  src="/visuals/partners/EykarLogo.svg"
-                  alt="Eykar logo"
-                />
-              </a>
-
-              <a href="https://focustree.app/">
-                <img
-                  className="m-5 rounded-xl"
-                  height={100}
-                  width={100}
-                  src="/visuals/partners/focustreeLogo.png"
-                  alt="Imperium Wars logo"
-                />{" "}
-              </a>
-
-              <a href="https://cartridge.gg/">
-                <CartridgeIcon />
-              </a>
-              <a href="https://imperiumwars.xyz/">
-                <img
-                  className="m-5 rounded-xl"
-                  height={100}
-                  width={100}
-                  src="/visuals/partners/ImperiumWarsLogo.png"
-                  alt="Imperium Wars logo"
-                />
-              </a>
-
-              <a href="https://www.frenslands.xyz/">
-                <img
-                  className="m-5 rounded-xl"
-                  height={120}
-                  width={120}
-                  src="/visuals/partners/FrenslandsLogo.png"
-                  alt="Frenslands logo"
-                />
-              </a>
-
-              <a href="https://www.earlystarkers.io/">
-                <img
-                  className="m-5 rounded-xl"
-                  height={180}
-                  width={180}
-                  src="/visuals/partners/EarlyStarkersLogo.svg"
-                  alt="Early Starkers logo"
-                />
-              </a>
-
-              <a href="https://realmseternum.com/">
-                <RealmsIcon />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.section3}>
-        <div className={styles.thirdLeaf}>
-          <img width={"100%"} alt="leaf" src="/leaves/leaf_2.png" />
-        </div>
-        <div className={styles.fourthLeaf}>
-          <img width={"100%"} alt="leaf" src="/leaves/leaf_1.png" />
-        </div>
-        <Roadmap />
-      </div>
-      <Footer />
-    </div>
+    </>
   );
 };
 
