063b4aefe6231700d40a755581f32f3336dec5cf
Fricoben <benjamin.flores.pro@gmail.com>
2023-04-11 14:56:10 +0200

    ref: add roadmap things

--- components/roadmap.tsx
+++ components/roadmap.tsx
@@ -33,12 +33,24 @@ const Roadmap: FunctionComponent = () => {
         <p className="text-center text-xl line-through">
           First block explorer integration
         </p>
-        <p className="text-center text-xl">New Starknet SBT implementation</p>
-        <p className="text-center text-xl">ENS Bridge</p>
-        <p className="text-center text-xl">Affiliation system</p>
+        <p className="text-center text-xl line-through">
+          Massive Subdomain distribution (free)
+        </p>
+        <p className="text-center text-xl line-through">
+          Starknetid SDK for developers
+        </p>
+        <p className="text-center text-xl">The Stark Tribe Campaign</p>
+        <p className="text-center text-xl">Automatic Renewal Feature</p>
+        <p className="text-center text-xl">Proof of PersonHood/KYC features</p>
       </div>
       <div className="my-5">
         <h2 className={styles.secondTitle}>Phase 3 : MATURATION</h2>
+        <p className="text-center text-xl">Cairo 1 migration</p>
+        <p className="text-center text-xl">ENS Bridging</p>
+        <p className="text-center text-xl">NFT Profile pic linking</p>
+        <p className="text-center text-xl">On-chain resolving</p>
+        <p className="text-center text-xl">Affiliation system</p>
+
         <p className="text-center text-xl">More features around identity TBA</p>
       </div>
     </div>
