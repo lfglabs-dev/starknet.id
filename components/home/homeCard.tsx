1a399a40110cbfb087150fe8a1a124a4f7c14782
nicolasito1411 <60229704+Marchand-Nicolas@users.noreply.github.com>
2023-12-13 16:29:09 +0100

    some responsive improvements

--- components/home/homeCard.tsx
+++ components/home/homeCard.tsx
@@ -16,7 +16,13 @@ const HomeCard: FunctionComponent<HomeCardProps> = ({
   return (
     <div className={styles.card}>
       <div className="flex justify-center">
-        <CDNImg alt="illustration" height={300} width={300} src={img} />
+        <CDNImg
+          className={styles.homeCardBanner}
+          alt="illustration"
+          height={300}
+          width={300}
+          src={img}
+        />
       </div>
       <h2 className={styles.thirdTitle}>{title}</h2>
       <div className="text-md max-w-3xl mt-4 sm:mt-10">
