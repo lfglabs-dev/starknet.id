b7876aa271e71e8a255c97f5080bc5a1c65891ea
johnsmccain <johnsdanlami@gmail.com>
2025-01-25 08:49:41 +0100

    Fix: Automatate slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,13 +11,13 @@ module.exports = {
       },
     },
     animation: {
-      marquee: "scroll 10s linear infinite",
+      marquee: "scroll 30s linear infinite",
     },
 
     keyframes: {
       scroll: {
         "0%": { transform: "translateX(0)" },
-        "100%": { transform: "translateX(-50%)" },
+        "100%": { transform: "translateX(-100%)" },
       },
     },
   },
