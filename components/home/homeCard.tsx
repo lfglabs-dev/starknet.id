60cc89e440628a70460b73575ee9d175ca361ec3
chiscookeke11 <chiscookeke11@gmail.com>
2025-01-26 09:25:38 -1100

    FIX-77

--- components/home/homeCard.tsx
+++ components/home/homeCard.tsx
@@ -18,7 +18,7 @@ const HomeCard: FunctionComponent<HomeCardProps> = ({
   mobileimg,
 }) => {
   return (
-    <div className="w-full bg-white max-w-[864px]  flex items-stretch justify-between flex-wrap m-4 mb-12  text-left text-inherit no-underline rounded-lg shadow-xl transition-all duration-150 ease-in-out bg-background-light z-10">
+    <div className="w-full overflow-hidden bg-white max-w-[864px]  flex items-stretch justify-between flex-wrap m-4 mb-12  text-left text-inherit no-underline rounded-lg shadow-xl transition-all duration-150 ease-in-out bg-background-light z-10">
       <div className="min-w-[300px] flex flex-col items-center lg:items-start gap-4 px-3 py-10 lg:pl-12 flex-1 lg:basis-1/2 text-center lg:text-left ">
         <h2 className="text-[#fff] bg-[#402D28] w-7 h-6 p-2 rounded-md text-sm font-medium text-center flex items-center justify-center ">
           {numb}
