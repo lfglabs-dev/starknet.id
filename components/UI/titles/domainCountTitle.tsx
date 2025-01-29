4022aa1ad5b5530923c1190756a06d3af69a71fa
Nico <60229704+Marchand-Nicolas@users.noreply.github.com>
2025-01-29 22:15:04 +0100

    remove console.log

--- components/UI/titles/domainCountTitle.tsx
+++ components/UI/titles/domainCountTitle.tsx
@@ -25,7 +25,6 @@ const DomainCountTitle: FunctionComponent<CategoryTitleProps> = ({
         );
         if (response.ok) {
           const data = await response.json();
-          console.log(data);
           setDomainCount(data.count);
         }
       } catch (error) {
