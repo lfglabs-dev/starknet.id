83defb193dcb275f0111ebd98e464ddd412a74d8
ayo-ola0710 <israelolatunle2015@gmail.com>
2025-02-11 21:05:57 +0100

    updated affliate page

--- components/affiliates/affiliateSection1.tsx
+++ components/affiliates/affiliateSection1.tsx
@@ -18,23 +18,23 @@ const AffiliateSection1: FunctionComponent = () => {
             className={styles.banner}
             width={380}
           />
-          <img alt="Ethereum icon" src="/icons/ethIcon.svg" className={`${styles.floatingIcons} ${styles.eth}`} width={37} />
+          <img alt="Ethereum icon" src="/icons/ethIcon.svg" className={`${styles.floatingIcons} ${styles.eth}`} width={40} />
           <img alt="Ethereum icon" src="/icons/ethIcon.svg" className={`${styles.floatingIcons} ${styles.smallEth}`} width={25} />
-          <img alt="Starknet icon" src="/icons/strkIcon.svg" className={`${styles.floatingIcons} ${styles.strk}`} width={32} />
+          <img alt="Starknet icon" src="/icons/strkIcon.svg" className={`${styles.floatingIcons} ${styles.strk}`} width={37} />
           <img alt="Starknet icon" src="/icons/strkIcon.svg" className={`${styles.floatingIcons} ${styles.smallStrk}`} width={18} />
         </div>
         <div className={styles.content}>
           <p className={styles.secondSubtitle}>Join & Earn!</p>
           <h1 className={styles.title}>The Stark Affiliate Program</h1>
           <div className={styles.highlights}>
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 text-sm">
               <CheckIcon width="16" color="#402D28" />
               Simple Steps
             </div>
             <div className={styles.separator} />
-            <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2 text-sm">
               <Money width="16" color="#402D28" />
-              Big Gains!
+              Big Gains !
             </div>
           </div>
           <h3 className={styles.subtitle}>
@@ -45,9 +45,9 @@ const AffiliateSection1: FunctionComponent = () => {
             <Button
               onClick={() => window.open("https://affiliate.starknet.id/")}
             >
-              <div className="flex items-center gap-6">
-                <p className="text-lg font-normal">Start earning now</p>
-                <RightArrow width="28" color="white" />
+              <div className="flex items-center gap-3">
+                <p className="text-[12px] font-normal">Start earning now</p>
+                <RightArrow width="18" color="white" />
               </div>
             </Button>
           </div>
