a255f5c74434fd63c7c415ae64f6b7536af4173d
JamesVictor-o <victorjames40v@gmail.com>
2025-02-24 09:47:03 +0100

    implement the the game of the year section

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,19 +29,19 @@ export default function Footer() {
                 <div className="flex flex-col items-center md:items-start space-y-3">
                   <Link
                     href="https://docs.starknet.id/"
-                    className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                    className="opacity-80 font-light hover:opacity-55 "
                   >
                     Documentation
                   </Link>
                   <Link
                     href="/pdfs/Terms.pdf"
-                     className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                     className="opacity-80 font-light hover:opacity-55"
                   >
                     Terms of use
                   </Link>
                   <Link
                     href="/pdfs/PrivacyPolicy.pdf"
-                     className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                     className="opacity-80 font-light  hover:opacity-55"
                   >
                     Privacy policy
                   </Link>
@@ -56,27 +56,27 @@ export default function Footer() {
                     target="_blank"
                     rel="noopener noreferrer"
 
-                     className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                     className="opacity-80 font-light  hover:opacity-55 "
 
                   >
                     Get Your Domain
                   </Link>
                   <Link
                     href="/"
-                     className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                     className="opacity-80 font-light  hover:opacity-55"
                   >
                     PFP Collections
                   </Link>
                   <Link
 
                     href="/"
-                    className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                    className="opacity-80 font-light  hover:opacity-55 "
                   >
                     Newsletter
                   </Link>
                   <Link
                     href="/affiliates/individual-program"
-                    className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+                    className="opacity-80 font-light  hover:opacity-55"
                     target="_blank"
                     rel="noopener noreferrer"
                   >
@@ -97,7 +97,7 @@ export default function Footer() {
               href="https://twitter.com/Starknet_id"
               target="_blank"
               rel="noopener noreferrer"
-              className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+              className="opacity-80 font-light  hover:opacity-55"
             >
               <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
@@ -108,7 +108,7 @@ export default function Footer() {
               href="https://discord.com/invite/8uS2Mgcsza"
               target="_blank"
               rel="noopener noreferrer"
-              className="opacity-80 font-light transition-all duration-300 hover:opacity-55 hover:translate-y-[-2px]"
+              className="opacity-80 font-light  hover:opacity-55"
             >
               <svg className="w-6 h-6" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M20.317 4.492c-1.53-.69-3.17-1.2-4.885-1.49a.075.075 0 0 0-.079.036c-.21.369-.444.85-.608 1.23a18.566 18.566 0 0 0-5.487 0 12.36 12.36 0 0 0-.617-1.23A.077.077 0 0 0 8.562 3c-1.714.29-3.354.8-4.885 1.491a.07.07 0 0 0-.032.027C.533 9.093-.32 13.555.099 17.961a.08.08 0 0 0 .031.055 20.03 20.03 0 0 0 5.993 2.98.078.078 0 0 0 .084-.026 13.83 13.83 0 0 0 1.226-1.963.074.074 0 0 0-.041-.104 13.175 13.175 0 0 1-1.872-.878.075.075 0 0 1-.008-.125c.126-.093.252-.19.372-.287a.075.075 0 0 1 .078-.01c3.927 1.764 8.18 1.764 12.061 0a.075.075 0 0 1 .079.009c.12.098.245.195.372.288a.075.075 0 0 1-.006.125c-.598.344-1.22.635-1.873.877a.075.075 0 0 0-.041.105c.36.687.772 1.341 1.225 1.962a.077.077 0 0 0 .084.028 19.963 19.963 0 0 0 6.002-2.981.076.076 0 0 0 .032-.054c.5-5.094-.838-9.52-3.549-13.442a.06.06 0 0 0-.031-.028zM8.02 15.278c-1.182 0-2.157-1.069-2.157-2.38 0-1.312.956-2.38 2.157-2.38 1.21 0 2.176 1.077 2.157 2.38 0 1.312-.956 2.38-2.157 2.38zm7.975 0c-1.183 0-2.157-1.069-2.157-2.38 0-1.312.955-2.38 2.157-2.38 1.21 0 2.176 1.077 2.157 2.38 0 1.312-.946 2.38-2.157 2.38z" />
