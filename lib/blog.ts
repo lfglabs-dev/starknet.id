48fb7440a2dfee9caa50a52f86667f157088e80f
Fricoben <benjamin.flores.pro@gmail.com>
2025-10-11 13:56:11 +0800

    fix: build

--- lib/blog.ts
+++ lib/blog.ts
@@ -5,7 +5,7 @@ import { ReactElement } from "react"
 import authorsData from "../lib/authors.json"
 import { customComponents } from "../mdx-components"
 
-const postsDirectory = path.join(process.cwd(), "pages/blog/posts")
+const postsDirectory = path.join(process.cwd(), "public/blog/posts")
 
 function estimateReadingTime(text: string): string {
   // Remove YAML frontmatter if present
